8695df30a43f96ea1a55d69b7d5b1f41758497fa
wasandev <o_wasan@hotmail.com>
2018-12-15 10:07:25 +0700

    nav2

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,5 +1,5 @@
 
-<nav class="bg-white  text-grey-lightest">
+<nav class=" bg-blue-light text-white">
     <div class="flex justify-between items-baseline py-6 px-8">
         <div>
             <a href="/" class="no-underline hover:text-black transition">
@@ -9,13 +9,13 @@
         </div>
         <div>
             @auth
-                <a href="{{ url('/') }}" class="text-grey-darker hover:text-black no-underline text-lg">
+                <a href="{{ url('/') }}" class="text-white hover:text-black no-underline text-white">
                     Home
                 </a>    
                 <cart-icon></cart-icon>
             @else
-                <a href="{{ route('login') }}" class="mr-2 no-underline text-grey-dark hover:text-grey-darkest transition text-lg mr-3">Login</a>
-                <a href="{{ route('register') }}" class="mr-2 no-underline text-grey-dark hover:text-grey-darkest transition text-lg mr-3">Register</a>
+                <a href="{{ route('login') }}" class="mr-2 no-underline text-white hover:text-grey-darkest transition text-lg mr-3">Login</a>
+                <a href="{{ route('register') }}" class="mr-2 no-underline text-white hover:text-grey-darkest transition text-lg mr-3">Register</a>
                 <cart-icon></cart-icon>
             @endauth   
             
