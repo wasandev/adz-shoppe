0d0b1a82e6e717d0cc69d3bb29694cab8808cb84
Wasan Ongart <o_wasan@hotmail.com>
2018-12-17 09:00:07 +0700

    PWA 1

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,6 +16,18 @@
         <script src="{{ mix('/js/vendor.js') }}" defer></script>
         <script src="{{ mix('/js/app.js') }}" defer></script>
 
+        <script>
+            // Check that service workers are registered
+            if ('serviceWorker' in navigator) {
+            // Use the window load event to keep the page load performant
+            window.addEventListener('load', () => {
+                navigator.serviceWorker.register('/sw.js');
+            });
+            }
+        </script>
+
+        <link rel="manifest" href="/manifest.json">
+
         <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
     </head>
     <body class="font-prompt" ">
