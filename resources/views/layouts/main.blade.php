b42b741c9d60d1e21c23584f37c9cbaf09a8981d
Wasan Ongart <o_wasan@hotmail.com>
2018-12-19 10:57:00 +0700

    update work box

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -17,13 +17,17 @@
         <script src="{{ mix('/js/app.js') }}" defer></script>
         @if (App::environment('production', 'staging'))
             <script>
-                // Check that service workers are registered
                 if ('serviceWorker' in navigator) {
-                // Use the window load event to keep the page load performant
-                window.addEventListener('load', () => {
-                    navigator.serviceWorker.register('/sw.js');
-                });
-                }
+                    window.addEventListener('load', function() {
+                        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+                        // Registration was successful
+                        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                        }, function(err) {
+                        // registration failed :(
+                        console.log('ServiceWorker registration failed: ', err);
+                        });
+                    });
+                    }
             </script>
         @endif
 
--- workbox-config.js
+++ workbox-config.js
@@ -1,18 +1,11 @@
-module.exports = {
-    cacheId: 'hr-app-' + Math.floor(Date.now() / 1000),
-    globDirectory: "public/",
-    globPatterns: [
-        "**/*.{css,ico,eot,svg,ttf,woff,woff2,js,json}",
-        "images/*.{png,jpg,jpeg,gif,bmp}",
-    ],
-
-    swDest: "public\\sw.js",
-    // Define runtime caching rules.
-    runtimeCaching: [{
-            urlPattern: "/",
-            handler: 'networkFirst',
-
-        },
-
-    ],
-};
+export const cacheId = 'hr-app-' + Math.floor(Date.now() / 1000);
+export const globDirectory = "public/";
+export const globPatterns = [
+    "**/*.{css,ico,eot,svg,ttf,woff,woff2,js,json}",
+    "images/*.{png,jpg,jpeg,gif,bmp}",
+];
+export const swDest = "public\\sw.js";
+export const runtimeCaching = [{
+    urlPattern: "/",
+    handler: 'networkFirst',
+}, ];
