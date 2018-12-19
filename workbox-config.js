f21434172b1258160ef69277f4510a286ef3af82
Wasan Ongart <o_wasan@hotmail.com>
2018-12-19 11:36:18 +0700

    update  pwa testing

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,7 @@
-export const cacheId = 'hr-app-' + Math.floor(Date.now() / 1000);
-export const globDirectory = "public/";
-export const globPatterns = [
-    "**/*.{css,ico,eot,svg,ttf,woff,woff2,js,json}",
-    "images/*.{png,jpg,jpeg,gif,bmp}",
-];
-export const swDest = "public\\sw.js";
-export const runtimeCaching = [{
-    urlPattern: "/",
-    handler: 'networkFirst',
-}, ];
+module.exports = {
+  "globDirectory": "public/",
+  "globPatterns": [
+    "**/*.{css,ico,php,js,json,txt,jpeg,svg}"
+  ],
+  "swDest": "public\\sw.js"
+};
