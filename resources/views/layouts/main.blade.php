800eb6cd47aa57a21d5aca59d5decc798c6cfa6f
Wasan Ongart <o_wasan@hotmail.com>
2018-12-19 10:43:54 +0700

    update PWA

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,16 +15,17 @@
         <script src="{{ mix('/js/manifest.js') }}" defer></script>
         <script src="{{ mix('/js/vendor.js') }}" defer></script>
         <script src="{{ mix('/js/app.js') }}" defer></script>
-
-        <script>
-            // Check that service workers are registered
-            if ('serviceWorker' in navigator) {
-            // Use the window load event to keep the page load performant
-            window.addEventListener('load', () => {
-                navigator.serviceWorker.register('/sw.js');
-            });
-            }
-        </script>
+        @if (App::environment('production', 'staging'))
+            <script>
+                // Check that service workers are registered
+                if ('serviceWorker' in navigator) {
+                // Use the window load event to keep the page load performant
+                window.addEventListener('load', () => {
+                    navigator.serviceWorker.register('/sw.js');
+                });
+                }
+            </script>
+        @endif
 
         <link rel="manifest" href="/manifest.json">
 
