a22a13e543709350e6680d938c21ad3eb956fd93
Wasan Ongart <o_wasan@hotmail.com>
2019-02-02 20:21:10 +0700

    update nav 2

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -7,11 +7,11 @@
             <span class="font-semibold text-xl tracking-tight">SISAHY GO</span>
             </div>
         
-        <div class="block lg:hidden">
+        {{-- <div class="block lg:hidden">
             <button @click="toggle" class="flex items-center px-3 py-2 border rounded text-red-light border-grey-light hover:text-grey-light hover:border-white">
                 <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
             </button>
-        </div>
+        </div> --}}
         <div :class="open ? 'block': 'hidden'" class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
             <div class="text-sm lg:flex-grow">
    
