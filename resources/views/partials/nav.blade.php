d084fce623cf5fca0dd96c135fea5d4eb23d531b
Wasan Ongart <o_wasan@hotmail.com>
2019-02-06 15:56:55 +0700

    update nav logo to svg

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,11 +1,13 @@
 
     <nav class="flex items-center justify-between shadow flex-wrap bg-white p-2" >
-        <div class="flex items-center flex-no-shrink text-red-light mr-8">
+        <div class="flex items-center flex-no-shrink text-indigo mr-8">
             
-            <img  class="fill-current h-12 w-12 mr-4" src="{{ Storage::url('logo.jpg') }}" />
-           
+            
+            <svg class="fill-current h-12 w-12 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M7 3H2v14h5V3zm2 0v14h9V3H9zM0 3c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3zm3 1h3v2H3V4zm0 3h3v2H3V7zm0 3h3v2H3v-2z"/>
+            </svg>
             <span class="font-semibold text-xl tracking-tight">SISAHY GO</span>
-            </div>
+        </div>
         
         <div class="block lg:hidden">
             <button @click="toggle" class="flex items-center px-3 py-2 border rounded text-red-light border-grey-light hover:text-grey-light hover:border-white">
